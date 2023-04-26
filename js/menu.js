a95071c59a3fc7dbe4a97e4930ec5a84bfaa6336
Krylova Nataliya <natalinebo2014@yandex.ru>
2023-04-26 22:49:45 +0200

    feat: add smooth scroll menu link on click

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,4 @@
-const menuArr = ["Moods", "Ideas", "Catalog", "Subscribe", "Contacts"];
+const menuArr = ["moods", "ideas", "catalog", "subscribe", "contacts"];
 const headerMenu = document.querySelector("#header-menu");
 const footerMenu = document.querySelector("#footer-menu");
 
@@ -12,7 +12,9 @@ function createMenuList(num) {
     const menuItem = document.createElement("li");
     menuItem.classList.add("menu__item");
     menuItem.innerHTML = `
-      <a href="#${menuArr[i]}" class="menu__link">${menuArr[i]}</a>
+      <a href="#${menuArr[i]}" class="menu__link">${menuArr[
+      i
+    ][0].toUpperCase()}${menuArr[i].slice(1)}</a>
     `;
 
     menuList.append(menuItem);
@@ -24,3 +26,18 @@ function createMenuList(num) {
 
 headerMenu.append(createMenuList(5));
 footerMenu.append(createMenuList(3));
+
+// choose all link that begin with # and set smooth scroll to the ancor sections
+
+const menuLinks = document.querySelectorAll('a[href^="#"]');
+menuLinks.forEach((link) => {
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+    const id = link.getAttribute("href");
+
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+});
